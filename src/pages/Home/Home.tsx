5ea7dfcf9c76f366bf51c9da4eeb8367533f9596
chicken <hibary.taiwan@gmail.com>
2022-09-17 11:42:20 +0800

    feat: add Nav

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,9 @@
-import { Header } from '../../components/home';
+import { Header,Nav } from '../../components/home';
 
 function Home() {
   return (
     <div className="bg-zinc-800">
+      <Nav/>
       <Header />
     </div>
   );
