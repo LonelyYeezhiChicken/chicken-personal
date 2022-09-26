b0868dbd927a6deb9a84965bc178c3cfcdf1ac31
chicken <hibary.taiwan@gmail.com>
2022-09-26 19:39:33 +0800

    feat: markdown table

--- src/components/common/PageBody/PageBody.tsx
+++ src/components/common/PageBody/PageBody.tsx
@@ -1,12 +1,7 @@
 import { PageBodyData } from "../../../models";
 import ReactMarkdown from "react-markdown";
 import { Link } from "react-router-dom";
-
-const tt = `
-| heading | b  |  c |  d  |
-| - | :- | -: | :-: |
-| cell 1 | cell 2 | 3 | 4 | 
-`;
+import remarkGfm from "remark-gfm";
 
 export function PageBody({ backPath, title, text }: PageBodyData) {
   return (
@@ -17,8 +12,11 @@ export function PageBody({ backPath, title, text }: PageBodyData) {
       <Link to={backPath} className="dark:text-yellow-50 text-gray-900 mb-10">
         <b>👈 回目錄</b>
       </Link>
-      <ReactMarkdown className="leading-9 text-gray-500 dark:text-white Md">
-        {tt}
+      <ReactMarkdown
+        className="leading-9 text-gray-500 dark:text-white Md"
+        remarkPlugins={[remarkGfm]}
+      >
+        {text}
       </ReactMarkdown>
       <footer className="m-10 mb-32">
         <Link to={backPath} className="dark:text-yellow-50 text-gray-900">
