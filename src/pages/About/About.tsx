01cce7e3280ebb066ec51fb458ba12d053e709f0
Elvina <qq861924@gmail.com>
2023-06-09 00:18:51 +0800

    fix: 🐛 about page css

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,39 +1,36 @@
-import { Header, Footer } from "../../components/home";
-import chicken from "../../assets/chicken.jpg";
-import { useTitle } from "../../utils/PageTitle";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
+import { Header, Footer } from '../../components/home';
+import chicken from '../../assets/chicken.jpg';
+import { useTitle } from '../../utils/PageTitle';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function About() {
-  useTitle("Chicken Say Hi | About");
+  useTitle('Chicken Say Hi | About');
   return (
     <>
       <section className="relative pt-16 bg-blueGray-50 max-h-full overflow-auto ">
         <div className="container mx-auto">
           <div className="flex flex-wrap items-center">
             <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
-              <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg dark:bg-amber-200 bg-amber-500">
+              <div className="flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg dark:bg-amber-200 bg-amber-500">
                 <LazyLoadImage
                   alt="..."
                   src={chicken}
                   className="w-full align-middle rounded-md max-h-62 p-5"
                   effect="blur"
                 />
-                <blockquote className="relative p-8 mb-4">
-                  <h4 className="text-xl font-bold text-white dark:text-gray-700 mb-6">
-                    孤獨一隻雞
-                  </h4>
-                  <p className="text-xl font-bold text-white dark:text-gray-700 mb-6">
+                <div className="p-5 mb-4 flex flex-col gap-y-3">
+                  <h4 className="text-xl font-bold text-white dark:text-gray-700">孤獨一隻雞</h4>
+                  <p className="text-xl font-bold text-white dark:text-gray-700">
                     I'm a backend developer.
                   </p>
-                  <p className="text-md font-light mt-2 text-gray-100 dark:text-gray-800">
-                    我就是一隻平凡無奇的雞，每天過著平凡無奇的生活
-                    <br />
+                  <p> 我是一隻平凡無奇的雞，每天過著平凡無奇的生活</p>
+                  <p className="cursor-pointer">
                     <a href="mailto:lonelyyeezhichicken@gmail.com">
                       e-mail: lonelyyeezhichicken@gmail.com
                     </a>
                   </p>
-                </blockquote>
+                </div>
               </div>
             </div>
 
@@ -95,9 +92,7 @@ function About() {
                       <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-gray-300 text-2xl text-gray-800">
                         📗
                       </div>
-                      <h6 className="text-xl mb-1 font-semibold">
-                        DB & Others
-                      </h6>
+                      <h6 className="text-xl mb-1 font-semibold">DB & Others</h6>
                       <ul className="mb-4 text-blueGray-500 list-decimal list-inside">
                         <li>MSSQL</li>
                         <li>MySQL</li>
