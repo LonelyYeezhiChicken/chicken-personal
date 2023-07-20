4cf0af2a39a9ee9e1a5a180b337dbb1ea67ec444
chicken <hibary.taiwan@gmail.com>
2023-07-20 21:03:09 +0800

    fix: banner color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,3 @@
-// import React from "react";
-// import clsx from "clsx";
-// import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-// import Layout from "@theme/Layout";
-
-// import styles from "./index.module.css";
-
-// function HomepageHeader() {
-//   const { siteConfig } = useDocusaurusContext();
-//   return (
-//     <header className={clsx("hero hero--primary", styles.heroBanner)}>
-//       <div className="container">
-//         <h1 className="hero__title">
-//           <img
-//             className={clsx(styles.titleLogo)}
-//             src={require("@site/static/img/chicken.jpg").default}
-//             alt="Image"
-//           />
-//         </h1>
-//         <h1 className="hero__subtitle">{siteConfig.tagline}</h1>
-//         <h1 className="hero__title">{siteConfig.title}</h1>
-//         {/* <div className={styles.buttons}>
-//           <Link
-//             className="button button--secondary button--lg"
-//             to="/docs/intro">
-//             Docusaurus Tutorial - 5min ⏱️
-//           </Link>
-//         </div> */}
-//       </div>
-//     </header>
-//   );
-// }
-
-// export default function Home(): JSX.Element {
-//   const { siteConfig } = useDocusaurusContext();
-//   return (
-//     <Layout
-//       title={`Hello from ${siteConfig.title}`}
-//       description="這是一隻雞的小天地"
-//     >
-//       <HomepageHeader />
-//       {/* <main>
-//         <HomepageFeatures />
-//       </main> */}
-//     </Layout>
-//   );
-// }
 import React, { useEffect } from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
@@ -146,9 +99,8 @@ function HomepageHeader() {
   }, []);
 
   return (
-    <header className={clsx(styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        {/* <img src={logo} style={{width: 280, borderRadius: '50%'}} />  */}
         <canvas width="300" height="300" style={{ borderRadius: "50%" }} />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
