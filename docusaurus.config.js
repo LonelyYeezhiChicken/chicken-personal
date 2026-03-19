837bd51807dbcae2c86ea939931c6c9d686e20b2
chicken <hibary.taiwan@gmail.com>
2026-03-19 15:35:10 +0800

    fix: migrate deprecated onBrokenMarkdownLinks to markdown config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,8 +22,7 @@ const config = {
   organizationName: "LonelyYeezhiChicken", // Usually your GitHub org/user name.
   projectName: "chicken-personal", // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -35,6 +34,7 @@ const config = {
 
   markdown: {
     mermaid: true,
+    onBrokenMarkdownLinks: "warn",
   },
 
   presets: [
